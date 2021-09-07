2caa6a7ac7bf9d198b97b500c6617d82d07a20e0
Ilya Gusev <phoenixilya@gmail.com>
2021-09-07 12:41:42 +0000

    fixes

--- headline_cause/util.py
+++ headline_cause/util.py
@@ -1,7 +1,11 @@
+import os
 import json
 import csv
+import random
 from urllib.parse import urlparse
 
+import torch
+import numpy as np
 
 def write_tsv(records, header, path):
     with open(path, "w") as w:
@@ -44,3 +48,15 @@ def get_host(url):
 
 def get_key(record):
     return tuple(sorted((record["left_url"], record["right_url"])))
+
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:2"
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
